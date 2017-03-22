9d13483a1d34e022d9444607372559ef44f122e8
lnpollola <leandro.pollola@hotmail.com>
2017-03-21 22:01:14 -0300

    Ejercicio 6 array

---
+++ GuiaEjer/Ejercicio6.php
@@ -0,0 +1,44 @@
+<?PHP
+
+//1) Con el constructor de array 
+//2) a.Crear un array con [] y agregar valores (0,1,2,3,4)
+//   b.Crear array con [] y asignarle valores random 
+//3) Crear con array_push () y un for 
+//4) Array asociativo (ver en php.net)
+
+//1)
+    // $arrayEjer6 = array(rand(),rand(),rand(),rand(),rand());
+    // var_dump($arrayEjer6);
+
+//2.a)
+    // for ($i=0;$i<5;$i++)
+    // {
+    //     $arrayEjer6[$i]=$i+1;
+    // }
+
+    //  var_dump($arrayEjer6);
+
+//2.b)
+    // for ($i=0;$i<5;$i++)
+    // { 
+    //     $arrayEjer6[$i+2]=$i+2;
+    //     $arrayEjer6[2]= 145;
+    // }
+    //  var_dump($arrayEjer6);
+
+//3)
+    //  $arrayEjer6 = array();
+    // array_push($arrayEjer6,rand(),rand(),rand(),rand(),rand());
+
+    // var_dump($arrayEjer6);
+//4)
+    // $arrayEjer6 = array("Numero1"=>"1","Numero2"=>"2","Numero3"=>"3","Numero4"=>"4","Numero5"=>"5" );
+
+    // echo $arrayEjer6["Numero1"];
+    // echo $arrayEjer6["Numero2"];
+    // echo $arrayEjer6["Numero3"];
+    // echo $arrayEjer6["Numero4"];
+    // echo $arrayEjer6["Numero5"];
+
+
+?>
