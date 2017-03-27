999c8bf5c9210ebcdb317d74197f8c3b96c73d67
lnpollola <leandro.pollola@hotmail.com>
2017-03-26 21:25:38 -0300

    EjerciciosHTML

--- GuiaEjer/Ejercicio5.php
+++ GuiaEjer/Ejercicio5.php
@@ -1,54 +1,72 @@
+<HTML>
+    <HEAD>
+    <TITLE> Ejercicio 5 </TITLE>
+    </HEAD>
+    <BODY>
 <?PHP 
-    $num = 57 ; //Del 20 al 60
 
-    switch (substr($num,0,1))
-    {
-        case '2':
-            echo "Veinte";
-            break;
-        case '3':
-            echo "Treinta";
-            break;
-        case '4':
-            echo "Cuarenta";
-            break;
-        case '5':
-            echo "Cincuenta";
-            break;
-        case '6':
-            echo "Sesenta";
-            breaK;
-    }
-
-        switch (substr($num,1,1))
+    switch (substr($_REQUEST['num'],0,1))
         {
-        case '1':
-            echo " y uno";
-            break;
-        case '2':
-            echo " y dos";
-            break;
-        case '3':
-            echo " y tres ";
-            break;
-        case '4':
-            echo " y cuatro";
-            breaK;
-        case '5':
-            echo " y cinco";
-            break;
-        case '6':
-            echo " y seis";
-            break;
-        case '7':
-            echo " y siete";
-            break;
-        case '8':
-            echo " y ocho";
-            break;
-        case '9':
-            echo " y nueve";
-            break;
+            case '2':
+                echo "Veinte";
+                break;
+            case '3':
+                echo "Treinta";
+                break;
+            case '4':
+                echo "Cuarenta";
+                break;
+            case '5':
+                echo "Cincuenta";
+                break;
+            case '6':
+                echo "Sesenta";
+                breaK;
         }
 
-?>
+            switch (substr($_REQUEST['num'],1,1))
+            {
+            case '1':
+                echo " y uno";
+                break;
+            case '2':
+                echo " y dos";
+                break;
+            case '3':
+                echo " y tres ";
+                break;
+            case '4':
+                echo " y cuatro";
+                breaK;
+            case '5':
+                echo " y cinco";
+                break;
+            case '6':
+                echo " y seis";
+                break;
+            case '7':
+                echo " y siete";
+                break;
+            case '8':
+                echo " y ocho";
+                break;
+            case '9':
+                echo " y nueve";
+                break;
+            }
+
+    ?>
+
+    
+
+    <form method="post" action="ProgInput.php">
+    Desea regresar s/n
+    <br>
+    <input type="radio" name="SI" value="s"> SI
+    <br>
+    <input type="radio" name="NO" value="n"> NO
+    <br>
+    <input type="submit" value="confirmar">
+    </form>
+    
+</HTML>
---
+++ GuiaEjer/ProgInput.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title> Programa para hacer de input </title>
+</head>
+<body>
+    <form method="post" action="Ejercicio5.php">
+    Ingrese un valor numerico:
+    <input type="text" name="num">
+    <br>
+    <input type="submit" value="confirmar">
+    </form>
+</body>
+</html>
