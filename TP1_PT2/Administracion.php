95b0fb7af3400782980b94e86c8b48158b92b7e5
lnpollola <leandro.pollola@hotmail.com>
2017-04-09 14:46:36 -0300

    Avance punto3
    Falta mostrar imagen en html nada mas

--- TP1_PT2/Administracion.php
+++ TP1_PT2/Administracion.php
@@ -7,29 +7,60 @@
         trim($_POST["VDNI"])=='' || 
         trim($_POST["VSexo"])==''  || 
         trim($_POST["VLegajo"])==''  || 
-        trim($_POST["VSueldo"])=='')
+        trim($_POST["VSueldo"])==''  
+        || trim($_FILES["VArchivo"]["name"])==''
+      )
     {
          echo "Todos los datos son obligatorios.\r\n";
          echo '<a href="index.html"> Volver a Ingresar</a>';
     }
     else 
     {
-         $empleado = new Empleado($_POST["VNombre"],$_POST["VApellido"],$_POST["VDNI"],$_POST["VSexo"],$_POST["VLegajo"],$_POST["VSueldo"]);
+      if 
+      (
+          (
+              ($_FILES["VArchivo"]["type"] == "image/jpg") ||
+                ($_FILES["VArchivo"]["type"] == "image/bmp") ||
+                ($_FILES["VArchivo"]["type"] == "image/gif") ||
+                ($_FILES["VArchivo"]["type"] == "image/png") ||
+                ($_FILES["VArchivo"]["type"] == "image/jpeg")
+          ) 
+          && ($_FILES["VArchivo"]["size"] < 1)
+          && !file_exists("fotos/". $_FILES["VArchivo"]["name"])
+      ) 
+        {
+            echo "Permitido el  archivo";
+            //Instancio el empleado 
+            $empleado = new Empleado($_POST["VNombre"],$_POST["VApellido"],$_POST["VDNI"],$_POST["VSexo"],$_POST["VLegajo"],$_POST["VSueldo"],$_POST["VArchivo"]);
+            
+            if (!file_exists("empleados.txt"))
+                {
+                    $archivoEmpleados= fopen("empleados.txt","w");
+                    fwrite($archivoEmpleados, $empleado->ToString()."\r\n" );
+                    echo "Se creo el archivo y  se guardo el empleado";
+                    echo '<a href="mostrar.php">Mostrar</a>';
+                }
+            else 
+                {
+                    $archivoEmpleados= fopen("empleados.txt","a");
+                    fwrite($archivoEmpleados, $empleado->ToString()."\r\n" );
+                    echo "El empleado se agrego correctamente";
+                    echo '<a href="mostrar.php">Mostrar</a>';
+                }
+            
+         }
+       else 
+       {
+          if ($_FILES["VArchivo"]["error"] > 0) 
+          {
+            echo $_FILES["VArchivo"]["error"] . "";
+          } 
+          else 
+          {
+            echo "Archivo no permitido";
+          }
+       }
          
-        if (!file_exists("empleados.txt"))
-        {
-            $archivoEmpleados= fopen("empleados.txt","w");
-            fwrite($archivoEmpleados, $empleado->ToString()."\r\n" );
-            echo "Se creo el archivo y  se guardo el empleado";
-            echo '<a href="mostrar.php">Mostrar</a>';
-        }
-        else 
-        {
-            $archivoEmpleados= fopen("empleados.txt","a");
-            fwrite($archivoEmpleados, $empleado->ToString()."\r\n" );
-            echo "El empleado se agrego correctamente";
-            echo '<a href="mostrar.php">Mostrar</a>';
-        }
  
     }
 
--- TP1_PT2/Empleado.php
+++ TP1_PT2/Empleado.php
@@ -4,17 +4,20 @@
     {
         //Atributos
         protected $_legajo;
+        protected $_pathFoto;
         protected $_sueldo;
 
-        function __construct($apellido,$nombre,$dni,$sexo,$legajo,$sueldo)
+        function __construct($apellido,$nombre,$dni,$sexo,$legajo,$sueldo,$pathFoto)
         {   
             parent::__construct($nombre,$apellido,$dni,$sexo);
             $this->_legajo=$legajo;
             $this->_sueldo=$sueldo;
+            $this->_pathFoto=$pathFoto;
         }
 
         //Getters
         public function getLegajo(){return $this->_legajo;}
+        public function getPathFoto(){return $this->_pathFoto;}
         public function getSueldo(){return $this->_sueldo;}
 
         //Funciones
@@ -23,11 +26,16 @@
             return "El empleado habla ".$idioma;
         }
 
+        public function setPathFoto($foto)
+        {
+            $this->_pathFoto = $foto;
+        }
         public function ToString()
         {
             return  parent::ToString() ."-".
                     $this->getLegajo() ."-".
-                    $this->getSueldo() ;
+                    $this->getSueldo() ."-".
+                    $this->getPathFoto();
         }
     }
     
