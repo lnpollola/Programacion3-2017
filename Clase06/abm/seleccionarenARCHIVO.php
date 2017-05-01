c2905db940a3277e67c820eead27b9eaeeb4de2d
lnpollola <leandro.pollola@hotmail.com>
2017-05-01 20:18:21 -0300

    ModificarArchivo
    Modif de archivo

--- Clase06/abm/seleccionarenARCHIVO.php
+++ Clase06/abm/seleccionarenARCHIVO.php
@@ -1,15 +1,20 @@
 <?PHP
     require_once "clases/producto.php";
 
-     $archivo = "archivo.txt"; 
+
+    $archivo = "productos.txt"; 
     $abrir = fopen($archivo, 'r+'); 
-    $contenido = fread($abrir, filesize($archivo)); 
+    $contenido = fread($abrir, filesize($archivo));
+
+    var_dump($contenido); 
     fclose($abrir); 
     $contenido2 = explode("\r\n", $contenido); 
+     var_dump($contenido2);
     //separamos la primera variable    de la fila 3     
-    $variable1 = explode("|", $contenido2[2]);  
+    //nombredeproducto
+    $variable1 = explode("-", $contenido2[2]);  
     $variable1[0] = "valor de la primera variable"; 
-    $variable1 = implode("|", $variable1);     
+    $variable1 = implode("-", $variable1);     
     //devolvemos el valor a la linea     
     $contenido2[2] = "$variable1";  //toda la fila 3 cambiada 
     //juntamos lo demas     
@@ -18,5 +23,5 @@
     fwrite($abrir, $nuevo); 
     fclose($abrir);  
     
-    require_once "modificarArchivo.php";
+    // require_once "modificarArchivo.php";
 ?>
